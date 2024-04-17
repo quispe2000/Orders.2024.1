cca5daf51d229ae3a0b7f3dd835ff4e95b3d1267
francisco <youremail>
2024-04-17 10:40:18 -0400

    Create database with the fist entity countries

---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracterr")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string Name { get; set; } = null!;
+    }
+}
